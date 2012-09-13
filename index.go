5875cedb47c278fb278b1bdb102ebb22e2c4d698
Alexandr Korsak <alex.korsak@gmail.com>
2012-09-13 23:05:59 +0300

    The first commit

---
+++ index.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "github.com/garyburd/twister/server"
+    "github.com/garyburd/twister/web"
+    "text/template"
+)
+
+var (
+  page *template.Template
+)
+
+func index(request *web.Request) {
+  page.Execute(
+    request.Respond(web.StatusOK, web.HeaderContentType, "text/html; charset=utf-8"),
+    request.URL.Host)
+}
+
+func main() {
+  page = template.Must(template.New("index").Parse(body))
+
+  server.Run(":3001",
+    web.NewRouter().
+      Register("/", "GET", index))
+}
+
+const body = `
+<html>
+  <head>
+    <title>Developed using Go :)</title>
+  </head>
+  <body>
+    <h1>I made this page using Go language</h1>
+  </body>
+</html> `
